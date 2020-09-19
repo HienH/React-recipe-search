2b0d766d2ed210960c4ab1050456ae65453f319c
hien hang <hienhang@hotmail.co.uk>
2020-09-19 01:12:28 +0100

    added no results fallback

--- src/App.js
+++ src/App.js
@@ -3,23 +3,26 @@ import './App.css';
 import Recipe from './Recipe';
 import { Container, Row, Col } from 'reactstrap';
 import { Button } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookOpen, faFrown } from '@fortawesome/free-solid-svg-icons'
 
 const App = () => {
 
+
     const [recipes, setRecipe] = useState([]);
     const [search, setSearch] = useState('');
-    const [query, setQuery] = useState('');
+    const [query, setQuery] = useState();
 
     useEffect(() => {
         getRecipes();
     }, [query]);
 
-
     const getRecipes = async () => {
         const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${process.env.REACT_APP_ID}&app_key=${process.env.REACT_APP_KEY}`);
         const data = await response.json();
-        console.log(data)
-        setRecipe(data.hits);
+        const recipes = data['hits']
+
+        setRecipe(recipes);
     }
 
     const updateSearch = e => {
@@ -33,8 +36,9 @@ const App = () => {
 
     return (
         <div className="text-center boarder">
-            <h1 className="font-grandstander title"> Recipe Search</h1>
-            <p className="font-grandstander font-20">This is a recipe search engine; Type in the ingredient you want to use up in the search box and we'll return a list of tasty recipes for you to enjoy.</p>
+            <h1 className="font-grandstander title"> Recipe Search <FontAwesomeIcon icon={faBookOpen} /></h1>
+            <p className="font-grandstander font-20">This is a recipe search engine; Type in the ingredient you want to use in the search box and we'll return a list of tasty recipes for you to enjoy.</p>
+
             <form onSubmit={getQuery}>
                 <input className="search-size font-20" type="text" name="search" value={search} onChange={updateSearch} />
                 <Button className="m-2 mb-3" type="submit" outline color="success">Seach</Button>
@@ -53,6 +57,7 @@ const App = () => {
                         </Col>
                     ))}
                 </Row>
+                {recipes.length === 0 && query ? <p className="font-bold font-20"> Sorry there are no recipies under the query: {query} </p> : null}
             </Container>
         </div>
     )
--- src/Recipe.js
+++ src/Recipe.js
@@ -3,13 +3,10 @@ import React from 'react';
 const Recipe = ({ title, image, recipeUrl }) => {
     return (
         <div className="">
-            <h4 className="font-grandstander  font-bold">{title}</h4>
-
-            <a href={recipeUrl} target="_blank">
+            <h4 className="font-grandstander font-bold">{title}</h4>
+            <a href={recipeUrl} target="_blank" rel="noopener noreferrer">
                 <img className="marginb-15" src={image} alt="recipe" />
             </a>
-
-
         </div>
     );
 };
